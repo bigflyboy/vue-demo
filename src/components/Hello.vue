c7bf80c921fc503f7f86369314a8604c8aa33baa
wangzhiyuan <wangzhiyuan@zuoyebang.com>
2019-11-29 14:33:40 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello2">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello2',
+    data() {
+      return{
+        msg: '欢迎来到菜鸟教程！'
+      }
+    }
+  }
+
+</script>
